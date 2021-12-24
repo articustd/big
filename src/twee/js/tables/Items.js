77263965ee5326c4c3592c137038e69852736600
Articus <articusthedata@gmail.com>
2021-12-23 21:58:31 -0800

    Added ability to use inventory items

--- src/twee/js/tables/Items.js
+++ src/twee/js/tables/Items.js
@@ -1,4 +1,4 @@
 let items = [
-    {name: "Test Item", mod: 1, type: "strg"},
-    {name: "Test Item 2", mod: 1, type: "strg"}
+    {name: "Test Item", mod: 1, type: "stats", stat: "strg"},
+    {name: "Test Item 2", mod: 1, type: "exp", stat: "size"}
 ]
