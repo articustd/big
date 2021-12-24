26c3908440ad578f7dfa5ccca6020ecaa3f3e908
Articus <articusthedata@gmail.com>
2021-12-24 15:40:55 -0800

    Loot tables, items, and prod ready

--- src/twee/js/tables/Enemies.js
+++ src/twee/js/tables/Enemies.js
@@ -1,4 +1,13 @@
-let enemies = [
+let testEnemies = [
     {name: "Test Enemy", species: "Test", stats:{maxHlth: 10, hlth: 10, strg: 5, def: 5, acc: 5, dex: 5}, exp:{muscle:[0,1],fat:[0,1],size:[0,1],skill:[0,1]}, height:[180,190], weight:[80,90], loot: 0},
     {name: "Test Enemy 2", species: "Test", stats:{maxHlth: 10, hlth: 10, strg: 5, def: 5, acc: 5, dex: 5}, exp:{muscle:[0,1],fat:[0,1],size:[0,1],skill:[0,1]}, height:[180,190], weight:[80,90], loot: 0}
 ];
+
+let enemies = [
+    [{name: "Muscle Mouse", species: "Mouse", stats:{maxHlth: 10, hlth: 10, strg: 10, def: 5, acc: 5, dex: 5}, exp:{muscle:[1,2],fat:[0,0],size:[0,1],skill:[0,1]}, height:[23,28], weight:[5,8], loot: 0},
+    {name: "Fat Mouse", species: "Mouse", stats:{maxHlth: 10, hlth: 10, strg: 10, def: 5, acc: 5, dex: 5}, exp:{muscle:[0,0],fat:[0,2],size:[0,1],skill:[0,1]}, height:[23,28], weight:[10,13], loot: 0}],
+    [{name: "Muscle Wolf", species: "Wolf", stats:{maxHlth: 20, hlth: 20, strg: 15, def: 5, acc: 5, dex: 5}, exp:{muscle:[1,4],fat:[0,1],size:[0,2],skill:[0,2]}, height:[180,190], weight:[80,90], loot: 0},
+    {name: "Fat Wolf", species: "Wolf", stats:{maxHlth: 20, hlth: 20, strg: 5, def: 15, acc: 5, dex: 5}, exp:{muscle:[0,1],fat:[1,4],size:[0,2],skill:[0,2]}, height:[180,190], weight:[100,105], loot: 0}],
+    [{name: "Muscle Dragon", species: "Dragon", stats:{maxHlth: 40, hlth: 40, strg: 15, def: 5, acc: 5, dex: 5}, exp:{muscle:[2,6],fat:[0,2],size:[0,4],skill:[0,3]}, height:[457,914], weight:[227,453], loot: 0},
+    {name: "Fat Dragon", species: "Dragon", stats:{maxHlth: 40, hlth: 40, strg: 5, def: 15, acc: 5, dex: 5}, exp:{muscle:[0,2],fat:[2,6],size:[0,4],skill:[0,3]}, height:[457,914], weight:[317,500], loot: 0}],
+]
--- src/twee/js/tables/Stores.js
+++ src/twee/js/tables/Stores.js
@@ -1,7 +1,7 @@
 let testStore = {
     name: "Test Store",
     stock: [
-        {id: 0, qty: 1, price: 80},
-        {id: 1, qty: 3, price: 90}
+        {id: 0, qty: 10, price: 50},
+        {id: 1, qty: 3, price: 200}
     ]
 }
