8098abdab47151d3f3bbec714e24280f6cecc3f0
Articus <articusthedata@gmail.com>
2022-02-22 22:28:21 -0800

    Moved JS, Added Sass to gulp, updated npm run commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 const   { src, dest, watch, series } = require('gulp'),
+        sass = require('gulp-sass')(require('sass')),
+        rename = require('gulp-rename'),
         { exec, spawn } = require('child_process'),
         { platform } = require('os'),
-        path = require('path');
-
-var command = `tweego${(platform() == 'win32')?'.exe':''}`, 
-    options = {cwd:path.resolve('vendor'), stdio: 'inherit'},   
-    args = ['--format=sugarcube-2', '--output=../dist/index.html', '../story/'];
+        path = require('path'),
+        command = `tweego${(platform() == 'win32')?'.exe':''}`, 
+        options = {cwd:path.resolve('vendor'), stdio: 'inherit'},   
+        args = ['--format=sugarcube-2', '--output=../dist/index.html', '../story/'];
 
 function buildTwee() {
     return spawn(command,args,options)
 }
 
-exports.buildTwee = buildTwee
-exports.default = function() {
-    watch('src/twee',{ignoreInitial: false},buildTwee)
+function buildSass() {
+    return src('src/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
+        .pipe(dest('story/modules'));
 }
+
+exports.buildTwee = buildTwee
+exports.buildSass = buildSass
+exports.buildDev = series(buildSass, buildTwee)
+exports.watchDev = function() {
+    watch('src/sass',{ignoreInitial: false},buildSass)
+    watch('story',buildTwee)
+}
+exports.default = this.watchDev
