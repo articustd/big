ec411b3c1c84831dd449b39e1aa48a87a6afd6d5
Articus <articusthedata@gmail.com>
2022-03-02 08:02:23 -0800

    Added boolean field and fixed some view issues

--- src/js/controller/util/StateEditor.js
+++ src/js/controller/util/StateEditor.js
@@ -5,17 +5,21 @@ export function stateEditor() {
     let $popup = $('<div id="stateEditor"/>').dialog({
         title: `State Editor`,
         resizeable: true,
+        width: 400,
+        height: 200,
         close: (event, ui)=>{$popup.remove()}
     })
 
     let $dropDown = createDropdown($popup, `Variable`, `testDropdow`)
+    let $fieldContainer = $('<div id="fieldContainer"/>')
+    $popup.append($fieldContainer)
     addOptions($dropDown, variables())
     $dropDown.selectmenu({
-        select: (event, ui) => { logger(variables()[ui.item.value]); addFields($popup, ui.item.value ,variables()[ui.item.value]) }
+        select: (event, ui) => { clearElement($fieldContainer); addField($fieldContainer, ui.item.value ,variables()[ui.item.value]) }
     })
 
     let firstItem = Object.keys(variables())[0]
-    addFields($popup, firstItem ,variables()[firstItem])
+    addField($fieldContainer, firstItem ,variables()[firstItem])
 
     return $popup
 }
@@ -24,7 +28,9 @@ function createDropdown($parent, label, id) {
     let $label = $(`<label for="${id}"/>`).wiki(`${label}: `)
     let $dropDown = $(`<select id="${id}" name="${id}"/>`)
 
-    $parent.append($label).append($dropDown)
+    let $wrapper = $('<div/>').append($label).append($dropDown).css({width: '100%',"margin-bottom":'10px'})
+
+    $parent.append($wrapper)
     $dropDown.selectmenu({
         width: 200
     })
@@ -38,7 +44,7 @@ function addOptions($dropDown, options) {
     $dropDown.selectmenu("refresh")
 }
 
-function addFields($parent, variable, varVal) {
+function addField($parent, variable, varVal) {
     logger(typeof variable)
     if (typeof varVal == "string") {
         let $label = $(`<label for="${variable}-text"/>`).wiki(`${variable}: `)
@@ -46,6 +52,19 @@ function addFields($parent, variable, varVal) {
             variables()[variable] = $(this).val()
             Engine.show()
         })
-        $parent.append($label).append($dropDown)
+        $parent.append($('<div/>').append($label).append($dropDown))
     }
+    if (typeof varVal == "boolean") {
+        let $label = $(`<label for="${variable}-text"/>`).wiki(`${variable}: `)
+        let $checkbox = $(`<input type="checkbox" id="${variable}-text" ${varVal?'checked':''}/>`).on('input',function (e){
+            variables()[variable] = $(this)[0].checked
+            Engine.show()
+        })
+
+        $parent.append($('<div/>').append($label).append($checkbox))
+    }
+}
+
+function clearElement($ele) {
+    $ele.empty()
 }
