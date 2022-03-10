1046f0d1dfb15039e74a0e880905d74ffaea0cdb
Articus <articusthedata@gmail.com>
2022-03-10 08:55:06 -0800

    Added text to travel and home passages about resting. Capacity bars now flash when over capacity with tool tip.

--- src/js/macro/CapacityBarMacro.js
+++ src/js/macro/CapacityBarMacro.js
@@ -4,17 +4,20 @@ Macro.add('capacityBarMacro', {
         let character = this.args[0]
         let capType = this.args[1]
 
-        let statusPercent = Math.floor((character.capacity[capType]/character.capacity[`${capType}Max`])*100)
-        
+        let statusPercent = Math.floor((character.capacity[capType] / character.capacity[`${capType}Max`]) * 100)
+
         let $capBar = $('<div/>')
-                            .addClass(`statusBar`)
+            .addClass(`statusBar`)
+        if (statusPercent > 100)
+            $capBar.tooltip({ track: true, hide: { delay: 100 } })
+                .attr('title', `Over capacity, get home and rest asap!`)
         let $currentCap = $('<div/>')
-                    .addClass(`${statusPercent<100?'currentNotVisCapBar':'currentVisCapBar'}`)
-                    .css('width', `${statusPercent}%`)
+            .addClass(`${statusPercent < 100 ? 'currentNotVisCapBar' : 'currentVisCapBar warningFlash'}`)
+            .css('width', `${statusPercent}%`)
         let $currentCapText = $('<div/>')
-                    .addClass('currentStatusText')
-                    .text(`${checkVisible(character, capType)}`)
-        
+            .addClass('currentStatusText')
+            .text(`${checkVisible(character, capType)}`)
+
         $capBar
             .attr('id', `macro-${this.name}`)
             .append($currentCap)
@@ -24,7 +27,7 @@ Macro.add('capacityBarMacro', {
 })
 
 function checkVisible(character, capType) {
-    if(character.capacity[capType]>character.capacity[`${capType}Max`])
+    if (character.capacity[capType] > character.capacity[`${capType}Max`])
         return `Visible`
     return `Not Visible`
 }
