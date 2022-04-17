53fb8efd5aeba91e0565f559c8a315b7666c5091
Articus <articusthedata@gmail.com>
2022-04-17 07:52:43 -0700

    Updated version and image

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ setup.ImagePath = "assets/";
 ((Config, State, Story, Engine, Dialog, $document) => {
 	$(document.head).append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">')
 	// Set State Variables
-	variables().version = `v0.8.0`
+	variables().version = `v0.8.1`
 	variables().items = items
 	variables().loot = loot;
 	variables().stores = stores;
