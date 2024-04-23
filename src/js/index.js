62e64387352828bf06b12ac9b1f10167fe75aec1
Articus <articusthedata@gmail.com>
2024-04-22 17:32:21 -0700

    Changed version number back to 0.12.0

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ setup.ImagePath = "assets/";
 
 ((Config, State, Story, Engine, Dialog, $document) => {
 	$(document.head).append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">')
-	let version = `v0.13.0`
+	let version = `v0.12.0`
 	// Set State Variables
 	variables().version = version
 	variables().stores = stores;
