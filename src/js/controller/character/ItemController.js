ed29ff94a8ef39446ff02126247dbea8564b6c3d
Articus <articusthedata@gmail.com>
2025-05-26 18:04:36 -0600

    Fixed pawn shop being able to sell along with some code documentation

--- src/js/controller/character/ItemController.js
+++ src/js/controller/character/ItemController.js
@@ -33,22 +33,55 @@ export function addToInventory({ id, qty }) { // FIXME Let me die father...
 	return qty
 }
 
-// function decreaseInventory(idx, inv) {
-// 	let { inv } = variables().player
-// 	inv[idx].qty -= 1
-// 	if (inv[idx].qty == 0)
-// 		inv.splice(idx, 1)
-// }
+/**
+ * Decreases the given entity's inventory. If an item quantity is reduced to 0 or below, it's removed from the entity's inventory array.
+ * 
+ * @param {Number} idx - Index of the item in the entity's inventory to decrease
+ * @param {Array} inv - Entity inventory to decrease item in
+ * @param {Number} quantity - Number of items to remove from inventory
+ */
+export function decreaseInventory(idx, inv, quantity) {
+	inv[idx].qty -= quantity
 
-function addCredits(credits) {
-	variables().player.credits += credits
-	return credits
+	if (inv[idx].qty <= 0)
+		inv.splice(idx, 1)
 }
 
+/**
+ * Adds credits to the given entity's holdings.
+ * 
+ * @param {Number} credits - Number of credits to add to the entity's holdings
+ * @param {Object} entity - Entity which will be receiving the credits, must have credits as a child property
+ * @returns {Number} Credits awarded to entity (Soon to be deprecated)
+ */
+function addCredits(credits, entity) {
+	entity.credits += credits
+}
+
+/**
+ * Returns an item object containing all of the item's data.
+ * 
+ * @param {Number} index - Index of the item in the ItemTable json
+ * @returns An item object containing the item data
+ */
 export function getItemInfoByIndex(index) {
 	return items[index]
 }
 
 export function decreaseCredits(amt) {
 	variables().player.credits -= amt;
+}
+
+/** 
+ * Allows the player to sell items.
+ * 
+ * @param {number} itemIdx - Item index in the seller's inventory
+ * @param {Array} seller - The seller, must have inv as a child property
+ * @param {number} price - Price seller is selling item for
+ * @param {number} quantity - Number to sell, default 1
+ * 
+ */
+export function sellItem(itemIdx, seller, price, quantity = 1) {
+	decreaseInventory(itemIdx, seller.inv, quantity)
+	addCredits(price * quantity, seller)
 }
--- src/js/macro/passages/city/PawnShopMacro.js
+++ src/js/macro/passages/city/PawnShopMacro.js
@@ -1,4 +1,4 @@
-import { addToInventory, decreaseCredits, getItemInfoByIndex } from "@controller/character/ItemController";
+import { addToInventory, decreaseCredits, getItemInfoByIndex, decreaseInventory, sellItem } from "@controller/character/ItemController";
 import { logger } from "@util/Logging"
 
 Macro.add('pawnShopMacro', {
@@ -14,7 +14,6 @@ Macro.add('pawnShopMacro', {
         })
 
         $.each(tableData, function (rowIndex, r) {
-            logger(getItemInfoByIndex(r[0]))
             if (rowIndex > 0) {
                 $table.append(
                     $('<div/>')
@@ -33,7 +32,7 @@ Macro.add('pawnShopMacro', {
                 )
                 $table.append(
                     $('<div/>')
-                        .wiki(getPawnPrice(r[0]) + "<<iconMacro 'credits' 'inline-icon light-text'>>")
+                        .wiki(getPawnPrice(r[0]) + " <<iconMacro 'credits' 'inline-icon light-text'>>")
                         .addClass('store-item-price')
                 )
                 //Single sell button
@@ -41,46 +40,36 @@ Macro.add('pawnShopMacro', {
                     .wiki(`Sell`)
                     .addClass('full-width')
                     .ariaClick(function (ev) {
-                        let storeText = ``
-                        if (r[1] > 0) { // If the item is in stock
-                            // if(State.variables.player.credits >= r[2]) { // Can the player afford it
-                            //     BuyItem(r, storeStock)
-                            //     storeText = `Bought 1 ${getItemInfoByIndex(r[0]).name}!`
-                            // } else
-                            //     storeText = `Not enough credits for ${getItemInfoByIndex(r[0]).name}`
-                            // Sell item to pawn shop
+                        let pawnText = ``
+                        if (r[1] > 0) { // Double check the player has the item to sell
+                            // Sell item to the pawn shop
+                            sellItem(r[2], variables().player, getPawnPrice(r[0]))
                             // Report result to pawnText
+                            pawnText = `Sold 1 ${getItemInfoByIndex(r[0]).name} for ${getPawnPrice(r[0]) * 1} <<iconMacro 'credits' 'inline-icon light-text'>>`
+                        } else // If somehow the player gets a sell button, but item is no longer in their inventory
+                            pawnText = `You don't have any ${getItemInfoByIndex(r[0]).name} to sell.`
 
-                        } else
-                            storeText = `${getItemInfoByIndex(r[0]).name} is not in stock`
-
-                        State.variables.storeText = storeText
+                        State.variables.storeText = pawnText
                         Engine.play(passage(), true)
                     })
                 $table.append($(`<div/>`).addClass('store-item-buy-single').append($button))
 
-                //Max buy button
+                //Max sell button
                 var $MaxButton = $(document.createElement('button'))
                     .wiki(`Sell Max`)
                     .addClass('full-width')
                     .ariaClick(function (ev) {
-                        let storeText = ``
-                        let SuccessCount = 0
+                        let pawnText = ``
                         //console.log(r[1])
-                        if (r[1] > 0) { // If the item is in stock
-                            for (let x = 0; x < r[1]; x++) {
-                                if (State.variables.player.credits >= r[2]) { // Can the player afford it
-                                    BuyItem(r, storeStock)
-                                    SuccessCount++;
-                                }
-                            }
-                            storeText = `Bought ` + SuccessCount + ` ${getItemInfoByIndex(r[0]).name}(s)`
-                        }
+                        if (r[1] > 0) { // Double check the player has the item to sell
+                            // Sell all of the item to the pawn shop
+                            sellItem(r[2], variables().player, getPawnPrice(r[0]), r[1])
+                            // Report result to pawnText
+                            pawnText = `Sold ${r[1]} ${getItemInfoByIndex(r[0]).name} for a total of ${(getPawnPrice(r[0]) * r[1])} <<iconMacro 'credits' 'inline-icon light-text'>>`
+                        } else // If somehow the player gets a sell button, but item is no longer in their inventory
+                            pawnText = `${getItemInfoByIndex(r[0]).name} is not in stock`
 
-                        else {
-                            storeText = `${getItemInfoByIndex(r[0]).name} is not in stock`
-                        }
-                        State.variables.storeText = storeText
+                        State.variables.storeText = pawnText
                         Engine.play(passage(), true)
                     })
 
@@ -109,10 +98,6 @@ function BuyItem(r, storeStock) {
     addToInventory({ id: r[0], qty: 1 })
     decreaseCredits(r[2])
     decreaseStock(r[3], storeStock)
-}
-
-function sellItem() {
-
 }
 
 function getPawnPrice(itemId) {
